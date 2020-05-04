97d43dbeff249ab3141efbb8ec944003fd292c04
Jonathan Mukai-Heidt <johnny@whiskermanor.com>
2020-05-04 10:39:31 -0400

    Create JobRun search-friendly index concurrently

--- explorer/src/migration/1588016794171-SpeedUpJobRunSearch.ts
+++ explorer/src/migration/1588016794171-SpeedUpJobRunSearch.ts
@@ -2,8 +2,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class SpeedUpJobRunSearch1588016794171 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(`
-        CREATE INDEX "job_run_searchable_addresses" ON "job_run" USING GIN
+    // NB: We have to do this in a different QueryRunner b/c the QueryRunner
+    // provided to us by the migration executor is already in a transaction.
+    // You can't run concurrent index creation inside a transaction.
+    const nonTransactionQueryRunner = queryRunner.connection.createQueryRunner()
+    await nonTransactionQueryRunner.query(`
+        CREATE INDEX CONCURRENTLY "job_run_searchable_addresses" ON "job_run" USING GIN
           ((ARRAY["job_run"."runId","job_run"."jobId","job_run"."requestId","job_run"."requester", "job_run"."txHash"]));
       `)
   }
